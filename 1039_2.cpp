4f8efdf467944747a75319482c2d03802d0c8913
Humbertzhang <504490160@qq.com>
2017-07-03 22:40:07 +0800

    1039 uncomplite

---
+++ 1039_2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class balls
+{
+public:
+	int ch[123];
+	balls(char *str)
+	{
+		int i;
+		
+		for(i=0;i<123;i++)
+			ch[i] = 0;
+
+		for(i=0;i<strlen(str);i++)
+			ch[str[i]] +=1;
+	}	
+
+};
+
+int main()
+{
+	char * instr,*needstr;
+	cin >> instr >> needstr;
+	
+	balls inballs(instr);
+	balls needballs(needstr);
+
+	int count_more=0,count_less=0,flag=1;
+
+	for(int i=0;i<123;i++){
+		if(inballs.ch[i] >= needballs.ch[i])
+			count_more += (inballs.ch[i]-needballs.ch[i]);
+		else
+		{
+			flag=0;
+			count_less+=(needballs.ch[i]-inballs.ch[i]);
+		}
+	}
+
+	if(flag == 0)
+		cout << "No " << count_less <<endl;
+	else if(flag == 1)
+		cout << "Yes " << count_more <<endl; 
+	return 0;
+}
