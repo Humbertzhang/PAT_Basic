82e5291e1ef014006c24e557a33942c8855c082d
Humbertzhang <504490160@qq.com>
2017-08-02 20:22:54 +0800

    1048.cpp

--- 1048.cpp
+++ 1048.cpp
@@ -1,104 +1,88 @@
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
+#include <cstring>
+#include <math.h>
 
 using namespace std;
+
+void get(char * a) //去除前导零
+{
+    int i=0,j=0,flag=0;
+    char temp;
+    for(i;;i++){
+        scanf("%c",&temp);
+
+        if(flag == 0) {
+            if(temp != '0')
+                flag = 1;
+        }
+        if(temp == ' ' || temp == '\n')
+            break;
+        if(flag ==1)
+            a[j++] = temp;
+    }
+    a[j] = '\0';
+}
+
 int main()
 {
-    int i,j,k;
-    char ina[101]={0};
-    char inb[101]={0};
+    char A[110],B[110];
+    get(A);
+    get(B);
 
-    cin >> ina >> inb;
+    //cin >> A >> B;
 
-    int lena,lenb,lenmax,lenabs,lenstart;
-    int flag;
+    int maxlength = max(strlen(A),strlen(B));
+    int n = strlen(A) - strlen(B);
+    int mid = abs(n);
 
-    lena = strlen(ina);
-    lenb = strlen(inb);
-    lenmax = lena>lenb?lena:lenb;
+    int flag = 0; //flag = 0 jishu  flag = 1 oushu
 
-    if(lenb >= lena){
-        flag = 1;
-        lenstart = lenmax;
-    }
+    int bitflag = 0; //0 : a> b  1 : b >a
+    if(strlen(B) >= strlen(A))
+        bitflag = 1;
+    else
+        bitflag = 0;
 
-    else{
-        flag = 2;
-        lenstart = lenb;
-    }
-    
-    lenabs = abs(lena-lenb);
+    for(int i=maxlength-1;i>mid-1;i--){
+        int a,b;
 
-    if(flag == 1){
-        for(i=lenstart-1;i>=lenabs;i--)
-        {
-            int a,b,temp;
-            a = ina[i]-'0';
-            b = inb[i-lenabs]-'0';
+        if(bitflag == 0)
+            a = A[i] - '0',b=B[i-mid]-'0';
+        else if(bitflag == 1)
+            a = A[i-mid]-'0',b=B[i]-'0';
 
-            if ((i+1)%2==0){
-                temp = b-a;
-                if(temp < 0)
-                    temp += 10;
-                inb[i] = '0'+temp;
-            }
+        int t=0;
+        char ct;
 
-            else if((i+1)%2==1){
-                temp = (a+b)%13;
-                if(temp <10){
-                    inb[i] = '0'+temp;
-                }
-                else if(temp == 10){
-                    inb[i] ='J';
-                }
-                else if(temp == 11){
-                    inb[i] ='Q';
-                }
-                else if(temp == 12){
-                    inb[i] ='K';
-                }
-            }
-
+        if(flag == 0){
+            t = (a + b)%13;
+            flag = 1;
         }
 
-    }
-
-
-    else if(flag == 2){
-        for(i=lenstart-1;i>=0;i--)
-        {
-            int a,b,temp;
-            a = ina[i+lenabs]-'0';
-            b = inb[i]-'0';
-
-            if ((i+1)%2==0){
-                temp = b-a;
-                if(temp < 0)
-                    temp += 10;
-                inb[i] = '0'+temp;
-            }
-
-            else if((i+1)%2==1){
-                temp = (a+b)%13;
-                if(temp <10){
-                    inb[i] = '0'+temp;
-                }
-                else if(temp == 10){
-                    inb[i] ='J';
-                }
-                else if(temp == 11){
-                    inb[i] ='Q';
-                }
-                else if(temp == 12){
-                    inb[i] ='K';
-                }
-            }
-
+        else if (flag == 1){
+            t = b-a;
+            if(t < 0)
+                t+=10;
+            flag =0;
         }
+
+        if (t == 10)
+            ct = 'J';
+        else if(t == 11)
+            ct = 'Q';
+        else if(t == 12)
+            ct = 'K';
+        else
+            ct = '0' + t;
+
+        if(bitflag == 1)
+            B[i] = ct;
+        else if(bitflag == 0)
+            B[i-mid] = ct;
     }
 
-    cout << inb << endl;
+    cout << B <<endl;
 
     return 0;
 }
+
